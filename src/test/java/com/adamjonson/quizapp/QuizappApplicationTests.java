2a98886089de525726190b2ca74763faf79b6919
Hp <adkhamjon.abd@gmail.com>
2024-04-04 16:39:43 +0200

    Initialize the project

---
+++ src/test/java/com/adamjonson/quizapp/QuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamjonson.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
