df76df20419fb99f74b5308f67c7293a6e3f0a59
Hp <adkhamjon.abd@gmail.com>
2024-04-06 11:16:20 +0200

    Add types of responses depending on request

--- src/main/java/com/adamjonson/quizapp/service/QuestionService.java
+++ src/main/java/com/adamjonson/quizapp/service/QuestionService.java
@@ -2,27 +2,40 @@ package com.adamjonson.quizapp.service;
 
 import com.adamjonson.quizapp.Question;
 import com.adamjonson.quizapp.dao.QuestionDao;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class QuestionService {
 
     QuestionDao questionDao;
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getAllQuestionsByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getAllQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
-        return "success";
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
 }
