731c8d5303fa992b519cfa2b09b12ecb62d95c8c
Hp <adkhamjon.abd@gmail.com>
2024-04-06 09:08:34 +0200

    Change the stategy of generating the primary key

--- src/main/java/com/adamjonson/quizapp/Question.java
+++ src/main/java/com/adamjonson/quizapp/Question.java
@@ -12,7 +12,7 @@ import org.hibernate.annotations.GeneratorType;
 
 public class Question {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String questionTitle;
     private String option1;
