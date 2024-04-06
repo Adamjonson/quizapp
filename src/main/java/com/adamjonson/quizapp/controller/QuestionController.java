6354d006d89704fbae1586d3c4d883d452641519
Hp <adkhamjon.abd@gmail.com>
2024-04-06 09:06:29 +0200

    Add new controller that posts data

--- src/main/java/com/adamjonson/quizapp/controller/QuestionController.java
+++ src/main/java/com/adamjonson/quizapp/controller/QuestionController.java
@@ -3,10 +3,7 @@ package com.adamjonson.quizapp.controller;
 import com.adamjonson.quizapp.Question;
 import com.adamjonson.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,11 @@ public class QuestionController {
     @GetMapping("category/{category}")
     public List<Question> getQuestionByCategory(@PathVariable String category){
         return questionService.getAllQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
 
     }
 }
--- src/main/java/com/adamjonson/quizapp/service/QuestionService.java
+++ src/main/java/com/adamjonson/quizapp/service/QuestionService.java
@@ -3,6 +3,9 @@ package com.adamjonson.quizapp.service;
 import com.adamjonson.quizapp.Question;
 import com.adamjonson.quizapp.dao.QuestionDao;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -16,5 +19,10 @@ public class QuestionService {
 
     public List<Question> getAllQuestionsByCategory(String category) {
         return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "success";
     }
 }
